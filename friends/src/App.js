62832bfab554dd22123606cbd21895d9c8ed1b11
Brian Kubes <briankubes@gmail.com>
2020-10-13 20:34:04 -0700

    findFriend is rendering

--- friends/src/App.js
+++ friends/src/App.js
@@ -5,14 +5,26 @@ import { Login } from './components/Login';
 import { BrowserRouter as Router, Link, Route } from "react-router-dom";
 import { PrivateRoute } from "./components/PrivateRoute";
 import {Friends} from "./components/Friends";
+import {FindFriend} from "./components/FindFriend";
 
 function App() {
   return (
     <Router>
       <h1>Friends App</h1>
+      <ul>
+        <li>
+        <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/friends">Friends</Link>
+        </li>
+        <li>
+          <Link to="/friends/id">Friends By Id</Link>
+        </li>
+      </ul>
     <div className="App">
-      <PrivateRoute exact path="/protected" component={Friends}/>
-      <Link to="/login">Login</Link>
+      <PrivateRoute exact path="/friends" component={Friends}/>
+      <PrivateRoute exact path="/friends/id" component={FindFriend}/>
       <Route exact path="/login" component={Login} />
     </div>
     </Router>
---
+++ friends/src/components/FindFriend.js
@@ -0,0 +1,43 @@
+import React, {useState, useEffect} from "react";
+import {axiosWithAuth} from "./utils/axiosWithAuth";
+
+
+export const FindFriend = () => {
+    const [findFriend, setFindFriend] = useState()
+    const [myFriend, setMyFriend] = useState({})
+    
+
+    const handleChange = (e) => {
+        setFindFriend(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        axiosWithAuth().get(`/friends/${findFriend}`)
+        .then((res)=>{
+            console.log("yes one friend", res.data)
+            setMyFriend(res.data)
+        })
+        .catch((err) =>{
+            console.log("erros getting one friend", err)
+        })
+    }
+
+    return(
+        <>
+        <h1>Test Find Friend</h1>
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="findFriend">Find Friend With Id#
+            <input
+            type="text"
+            name="findFriend"
+            onChange={handleChange}
+            value={findFriend}
+            ></input>
+            </label>
+            <button>Submit</button>
+        </form>
+        <h2>Name:{myFriend.name} Age:{myFriend.age} Id:{myFriend.id}</h2>
+        </>
+    )
+}
